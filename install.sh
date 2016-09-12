52441d488db4b9fd08a1a2141abc34892e162d32
Frieder Schlesier <fschl.code@gmail.com>
2016-09-12 11:00:50 +0200

    updates

--- install.sh
+++ install.sh
@@ -78,6 +78,8 @@ no_suspend() {
     # https://wiki.debian.org/SystemdSuspendSedation
     sudo sed -i "s/HandleLidSwitch=.*/HandleLidSwitch=ignore/" /etc/systemd/logind.conf
     sudo sed -i "s/HandleLidSwitchDocked=.*/HandleLidSwitchDocked=ignore/" /etc/systemd/logind.conf
+    sudo sed -i "s/IdleActionSec=.*/IdleActionSec=90min/" /etc/systemd/logind.conf
+
     sudo systemctl restart systemd-logind.service
 }
 
@@ -140,6 +142,91 @@ get_dotfiles() {
         git clone https://github.com/fschl/.emacs.d.git "/home/$USERNAME/.emacs.d"
     )
 }
+
+# install/update golang from source
+install_golang() {
+    export GO_VERSION=1.6.2
+    export GO_SRC=/usr/local/go
+
+    # if we are passing the version
+    if [[ ! -z "$1" ]]; then
+        export GO_VERSION=$1
+    fi
+
+    # subshell because we `cd`
+    (
+    curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz" | sudo tar -v -C /usr/local -xz
+    )
+
+    # get commandline tools
+    (
+    set -x
+    set +e
+    go get github.com/golang/lint/golint
+    go get golang.org/x/tools/cmd/cover
+    go get golang.org/x/review/git-codereview
+    go get golang.org/x/tools/cmd/goimports
+    go get golang.org/x/tools/cmd/gorename
+
+    go get github.com/FiloSottile/vendorcheck
+    go get github.com/nsf/gocode
+    #done
+    )
+}
+
+get_public_go_projects() {
+
+    (
+        aliases=( Masterminds/glide onsi/ginkgo onsi/gomega Compufreak345/alice Compufreak345/manners Compufreak345/go-i18n Compufreak345/excess-router Compufreak345/leaflet-map Compufreak345/jsencrypt gogits/gogs fschl/sql-migrate fschl/CompileDaemon )
+
+        for project in "${aliases[@]}"; do
+            owner=$(dirname "$project")
+            repo=$(basename "$project")
+            if [[ -d "${HOME}/${repo}" ]]; then
+                rm -rf "${HOME}/${repo}"
+            fi
+
+            mkdir -p "${GOPATH}/src/github.com/${owner}"
+
+            if [[ ! -d "${GOPATH}/src/github.com/${project}" ]]; then
+                (
+                    # clone the repo
+                    cd "${GOPATH}/src/github.com/${owner}"
+                    git clone "https://github.com/${project}.git"
+                    # fix the remote path, since our gitconfig will make it git@
+                    cd "${GOPATH}/src/github.com/${project}"
+                    git remote set-url origin "https://github.com/${project}.git"
+                )
+            else
+                echo "found ${project} already in gopath"
+            fi
+
+            # make sure we create the right git remotes
+            # if [[ "$owner" != "fschl" ]]; then
+            #   (
+            #   cd "${GOPATH}/src/github.com/${project}"
+            #   git remote set-url --push origin no_push
+            #   git remote add jfrazelle "https://github.com/fschl/${repo}.git"
+            #   )
+            # fi
+
+            # create the alias
+            # ln -snvf "${GOPATH}/src/github.com/${project}" "${HOME}/${repo}"
+        done
+
+        # create symlinks from personal projects to
+        # the ${HOME} directory
+        projectsdir=$GOPATH/src/github.com/fschl
+        base=$(basename "$projectsdir")
+        find "$projectsdir" -maxdepth 1 -not -name "$base" -type d -print0 | while read -d '' -r dir; do
+            base=$(basename "$dir")
+            ln -snvf "$dir" "${HOME}/${base}"
+
+        done
+    )
+}
+
+source get_private_stuff.sh
 
 main() {
     local cmd=$1
